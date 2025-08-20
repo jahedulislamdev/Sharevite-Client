ceb76ccbb02c39ad907e098bee6a6b65773676c3
JISHAN <jahedulislamdev@gmail.com>
2025-08-20 20:23:13 +0600

    added usescroll hook

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Components/Footer";
 import Nav from "../Components/Navbar";
+import UseScroll from "../hooks/useScroll";
 
 const MainLayout = () => {
    return (
       <div className="">
+         <UseScroll />
          <Nav />
          <Outlet />
          <Footer />
