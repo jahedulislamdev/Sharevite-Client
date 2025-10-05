681192e0cf4d44bb5c450a7e3e3e0d1640b52c5d
JISHAN <jahedulislamdev@gmail.com>
2025-10-05 22:17:07 +0600

    added useSecureAPi hook

--- src/hooks/useDeleteData.js
+++ src/hooks/useDeleteData.js
@@ -1,22 +1,35 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteRequest } from "../utils/apiClent";
 import { toast } from "sonner";
+import useSecureApi from "./useSecureApi";
 
-const useDeleteData = (ednpoint, successMessage, queryKey) => {
-   const queryClient = useQueryClient();
-   return useMutation({
-      mutationFn: (id) => deleteRequest(ednpoint, id),
-      onSuccess: (res) => {
-         console.log(res);
-         if (successMessage) {
-            toast.success(successMessage);
-         }
-         const keys = Array.isArray(queryKey) ? queryKey : [queryKey];
-         keys.forEach(key => queryClient.invalidateQueries(key));
+const useDeleteData = (
+    ednpoint,
+    successMessage,
+    queryKey,
+    isSecure = false,
+) => {
+    const queryClient = useQueryClient();
+    const { secureDeleteRequest } = useSecureApi();
+    return useMutation({
+        mutationFn: (id) => {
+            if (isSecure) {
+                secureDeleteRequest(ednpoint, id);
+            } else {
+                deleteRequest(ednpoint, id);
+            }
+        },
+        onSuccess: (res) => {
+            console.log(res);
+            if (successMessage) {
+                toast.success(successMessage);
+            }
+            const keys = Array.isArray(queryKey) ? queryKey : [queryKey];
+            keys.forEach((key) => queryClient.invalidateQueries(key));
 
-         return res;
-      }
-   })
+            return res;
+        },
+    });
 };
 
 export default useDeleteData;
